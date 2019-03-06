c39f0238e40d253b56672995597c91ec7a28f2ee
[JuliaOk] <[okuniewska711@gmail.com]>
2019-03-06 11:06:26 +0100

    Fantom wejść parametrów
    Makiety wejść wartości parametrów. Bez formatowania, bez niczego, jedynie makieta

--- build-MMM_PL_JO-Desktop_Qt_5_12_1_MSVC2017_32bit-Debug/ui_mainwindow.h
+++ build-MMM_PL_JO-Desktop_Qt_5_12_1_MSVC2017_32bit-Debug/ui_mainwindow.h
@@ -12,7 +12,10 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
@@ -27,10 +30,24 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QGraphicsView *graphicsView;
+    QGroupBox *groupBox;
+    QPushButton *pushButton_3;
     QPushButton *pushButton;
     QPushButton *pushButton_2;
-    QPushButton *pushButton_3;
+    QGroupBox *groupBox_2;
+    QWidget *horizontalLayoutWidget_2;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
     QLabel *label;
+    QLineEdit *lineEdit;
+    QGroupBox *groupBox_3;
+    QWidget *horizontalLayoutWidget_4;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_4;
+    QLineEdit *lineEdit_4;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -44,19 +61,79 @@ public:
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         graphicsView = new QGraphicsView(centralWidget);
         graphicsView->setObjectName(QString::fromUtf8("graphicsView"));
-        graphicsView->setGeometry(QRect(30, 330, 551, 261));
-        pushButton = new QPushButton(centralWidget);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(1020, 50, 131, 71));
-        pushButton_2 = new QPushButton(centralWidget);
-        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(1020, 130, 131, 71));
-        pushButton_3 = new QPushButton(centralWidget);
+        graphicsView->setGeometry(QRect(20, 140, 1111, 451));
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(20, 10, 471, 111));
+        pushButton_3 = new QPushButton(groupBox);
         pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-        pushButton_3->setGeometry(QRect(1020, 210, 131, 71));
-        label = new QLabel(centralWidget);
+        pushButton_3->setGeometry(QRect(310, 30, 131, 71));
+        pushButton = new QPushButton(groupBox);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(30, 30, 131, 71));
+        pushButton_2 = new QPushButton(groupBox);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(170, 30, 131, 71));
+        groupBox_2 = new QGroupBox(centralWidget);
+        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
+        groupBox_2->setGeometry(QRect(520, 10, 361, 111));
+        horizontalLayoutWidget_2 = new QWidget(groupBox_2);
+        horizontalLayoutWidget_2->setObjectName(QString::fromUtf8("horizontalLayoutWidget_2"));
+        horizontalLayoutWidget_2->setGeometry(QRect(210, 20, 131, 80));
+        horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget_2);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(horizontalLayoutWidget_2);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout_2->addWidget(label_2);
+
+        lineEdit_2 = new QLineEdit(horizontalLayoutWidget_2);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        horizontalLayout_2->addWidget(lineEdit_2);
+
+        horizontalLayoutWidget = new QWidget(groupBox_2);
+        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(10, 20, 131, 80));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(horizontalLayoutWidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(1020, 20, 131, 21));
+
+        horizontalLayout->addWidget(label);
+
+        lineEdit = new QLineEdit(horizontalLayoutWidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+        groupBox_3 = new QGroupBox(centralWidget);
+        groupBox_3->setObjectName(QString::fromUtf8("groupBox_3"));
+        groupBox_3->setGeometry(QRect(900, 10, 231, 111));
+        horizontalLayoutWidget_4 = new QWidget(groupBox_3);
+        horizontalLayoutWidget_4->setObjectName(QString::fromUtf8("horizontalLayoutWidget_4"));
+        horizontalLayoutWidget_4->setGeometry(QRect(10, 20, 211, 80));
+        horizontalLayout_4 = new QHBoxLayout(horizontalLayoutWidget_4);
+        horizontalLayout_4->setSpacing(6);
+        horizontalLayout_4->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
+        label_4 = new QLabel(horizontalLayoutWidget_4);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        horizontalLayout_4->addWidget(label_4);
+
+        lineEdit_4 = new QLineEdit(horizontalLayoutWidget_4);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+
+        horizontalLayout_4->addWidget(lineEdit_4);
+
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
@@ -77,10 +154,15 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        groupBox->setTitle(QApplication::translate("MainWindow", "pobudzenie u(t) :", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "sinusoida", nullptr));
         pushButton->setText(QApplication::translate("MainWindow", "fala prostok\304\205tna", nullptr));
         pushButton_2->setText(QApplication::translate("MainWindow", "skok jednostkowy", nullptr));
-        pushButton_3->setText(QApplication::translate("MainWindow", "sinusoida", nullptr));
-        label->setText(QApplication::translate("MainWindow", "pobudzenie u(t) :", nullptr));
+        groupBox_2->setTitle(QApplication::translate("MainWindow", "Edycja parametr\303\263w ai", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "a0 = ", nullptr));
+        label->setText(QApplication::translate("MainWindow", "a1 = ", nullptr));
+        groupBox_3->setTitle(QApplication::translate("MainWindow", "Edycja parametru op\303\263\305\272nienia transportowego", nullptr));
+        label_4->setText(QApplication::translate("MainWindow", "T = ", nullptr));
     } // retranslateUi
 
 };
