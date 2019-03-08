7567620a4ea06963f8eeb9ab07973d25c2980216
[PatkaL] <patrycja.lipkiewicz@o2.pl>
2019-03-08 22:10:55 +0100

    zczytywanie danych z przycisków a,b,T
    zczytanie danych, zapisanie do inta typu a_0, jeśli nie jest liczbą dodatnią to uwagi w okienku remarks

--- MMM_PL_JO/mainwindow.cpp
+++ MMM_PL_JO/mainwindow.cpp
@@ -27,4 +27,75 @@ void MainWindow::createTextTransmitation()
     textTransmitation.append(QString::number(paramA0));
 }
 
+void MainWindow::display_remarks()
+{
 
+    QString mistake="bledne wspolczynniki:";
+    if(a_0==0)
+    {
+        mistake += " a0";
+    }
+    if(a_1==0)
+    {
+        mistake += " a1";
+    }
+    if(b_0==0)
+    {
+        mistake += " b0";
+    }
+    if(b_1==0)
+    {
+        mistake += " b1";
+    }
+    if(T==0)
+    {
+        mistake += " T";
+    }
+    ui->remarks->setText(mistake);
+
+    if(a_0 != 0 && a_1 != 0 && b_0 != 0 && b_1 != 0 && T!=0)
+        ui->remarks->setText(" ");
+}
+
+void MainWindow::on_lineEdit_textChanged()
+{
+    QString a_1_string = ui->lineEdit->text();
+    bool convertOK;
+    a_1 = a_1_string.toInt(&convertOK);
+    display_remarks();
+
+}
+
+void MainWindow::on_lineEdit_2_textChanged()
+{
+    QString a_0_string = ui->lineEdit_2->text();
+    bool convertOK;
+    a_0 = a_0_string.toInt(&convertOK);
+    display_remarks();
+
+}
+
+void MainWindow::on_lineEdit_8_textChanged()
+{
+    QString b_1_string = ui->lineEdit_8->text();
+    bool convertOK;
+    b_1 = b_1_string.toInt(&convertOK);
+    display_remarks();
+}
+
+
+void MainWindow::on_lineEdit_3_textChanged()
+{
+    QString b_0_string = ui->lineEdit_3->text();
+    bool convertOK;
+    b_0 = b_0_string.toInt(&convertOK);
+    display_remarks();
+}
+
+void MainWindow::on_lineEdit_4_textChanged()
+{
+    QString T_string = ui->lineEdit_4->text();
+    bool convertOK;
+    T = T_string.toInt(&convertOK);
+    display_remarks();
+}
--- MMM_PL_JO/mainwindow.h
+++ MMM_PL_JO/mainwindow.h
@@ -23,10 +23,27 @@ public:
     int paramA1 = 10;
     int paramA0 = 10;
     int paramT  = 5;
+    int a_0;
+    int a_1;
+    int b_0;
+    int b_1;
+    int T;
 
   QString textTransmitation;
 
     void createTextTransmitation();
+    void display_remarks();
+
+private slots:
+    void on_lineEdit_textChanged();
+
+    void on_lineEdit_2_textChanged();
+
+    void on_lineEdit_8_textChanged();
+
+    void on_lineEdit_3_textChanged();
+
+    void on_lineEdit_4_textChanged();
 
 private:
     Ui::MainWindow *ui;
